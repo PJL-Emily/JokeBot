43e7602a972ede51cd460a18c54f3406d725f341
PJL-Emily <b07705053@ntu.edu.tw>
2021-12-06 07:31:26 +0800

    Connect mongoDB

--- app.py
+++ app.py
@@ -1,48 +1,32 @@
 import os
-from flask import Flask, abort, request, g
-from flask_script import Manager
+from dotenv import load_dotenv
+from flask import Flask, abort, request
 from datetime import datetime
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import sqlite3
+# pymongo
+from flask.json import JSONEncoder
+import pymongo
+from bson import json_util
+
+class MongoJSONEncoder(JSONEncoder):
+    def default(self, obj): 
+        return json_util.default(obj)
 
 app = Flask(__name__)
-manager = Manager(app)
+app.json_encoder = MongoJSONEncoder
+load_dotenv()
+MONGO_URI = os.getenv('MONGO_URI')
+client = pymongo.MongoClient(MONGO_URI)
+db = client.TourBot
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
-DATABASE = "database.db"
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-        # Enable foreign key check
-        db.execute("PRAGMA foreign_keys = ON")
-    return db
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-def remove_db():
-    if os.path.isfile(DATABASE):
-        os.remove(DATABASE) 
-
-if __name__ == '__main__':
-    manager.run()
+# line_bot_api = LineBotApi(config.get('line-bot', 'CHANNEL_ACCESS_TOKEN'))
+# handler = WebhookHandler(config.get('line-bot', 'CHANNEL_SECRET'))
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
@@ -59,33 +43,56 @@ def callback():
 
         return "OK"
 
-choice_work_text='JokeBot! 準備好沒，我要交付你待辦事項了!!'
-choice_ddl_text='JokeBot，一周內有哪些死線？我準備要起跑了！'
-choice_joke_text='JokeBot... 我累了... 來點 Joke...'
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
-    if get_message.find("完成了") != -1:
-        task_name = get_message[:get_message.find("完成了")]
-        # find task
-        # set task.isFinished = 1
-        reply = '恭喜完成' + task_name + '！你好棒～～'
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))
-    elif get_message == choice_work_text: 
-        reply = "好，來吧！首先跟我說待辦事項標題"
+    if db.chat_state.find_one({ "time": "current" }).state == "task":
+        step = db.chat_state.find_one({ "time": "current" }).step
+        reply = db.choices.find_one({ "name": 'task'}).sys_replies[step]
+        step += 1
+        if step >= len(db.choices.find_one({ "name": 'task'}).sys_replies):
+            result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "general", "step": 0 } })
+        else:
+            result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "task", "step": step } })
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))        
-    elif get_message == choice_ddl_text: 
-        reply = "一周內有：\n12/8 MDS proposal\n12/10 OR hw8"
-        reply += "\n你再不跑快點我看是完蛋"
+    # see if there's event from rich-menu
+    # choices = ['task', 'deadline', 'joke']:
+    elif get_message == db.choices.find_one({ "name": 'task' }).usr_utter:
+        result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "task", "step": 0 } })
+        reply = db.choices.find_one({ "name": 'task' }).sys_replies[0]
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))
-    elif get_message == choice_joke_text: 
-        reply = "文藝青年被噴防蚊液就變普通青年"
+    elif get_message == db.choices.find_one({ "name": 'dealine' }).usr_utter:
+        result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "deadline", "step": 0 } })
+        reply = "還沒完成的事項有：\n"
+        todos = ""
+        for t in db.tasks.find().sort([("deadline", pymongo.ASCENDING)]):
+            if not t.isFinished:
+                todos += t.deadline + ' ' + t.title + '\n'
+        if todos == "":
+            reply = "沒有尚未完成的事項，太強了吧！！！"
+        else: 
+            reply += db.choices.find_one({ "name": 'dealine' }).sys_reply
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))
-    else:
-        reply = "嗡嗡嗡 快上工！"
+    elif get_message == db.choices.find_one({ "name": 'joke' }).usr_utter:
+        result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "joke", "step": 0 } })
+        reply = db.jokes.find_one().content
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))
     
-    # Send To Line
-    reply = TextSendMessage(text=f"{get_message}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    # see if any finished task is reported
+    elif get_message.find("完成了") != -1:
+        result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "general", "step": 0 } })
+        task_name = get_message[:get_message.find("完成了")]
+        # find task
+        result = db.tasks.find_one({ "title": task_name })
+        if result is None:
+            reply = '確定沒打錯字？我怎麼不記得有' + task_name + '這個事項'
+        else:
+            result = db.tasks.update_one({ "title": task_name }, { "$set": { "isFinished": True } })
+            reply = '恭喜完成' + task_name + '！你好棒～～'
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))
+
+    # general case
+    else:
+        result = db.chat_state.update_one({ "time": "current" }, { "$set": { "state": "general", "step": 0 } })
+        reply = "嗡嗡嗡 快上工！"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=f"{reply}"))
